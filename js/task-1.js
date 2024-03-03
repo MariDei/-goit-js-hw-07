14ceb94cf0f60bdc6288166d5671ccba5a7005c5
Marianna Dei <d.marianna@icloud.com>
2024-03-03 17:59:52 +0200

    Added styles and script to task-1.js.

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const listCategories = document.querySelectorAll("#categories .item");
+
+console.log(`Number of categories: ${listCategories.length}`);
+
+listCategories.forEach(category => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryItems = category.querySelectorAll("li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItems}`);
+})
