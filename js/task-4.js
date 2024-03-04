a33a4e5caf013464ad9d6b88e7ec058be19de74c
Marianna Dei <d.marianna@icloud.com>
2024-03-04 22:33:11 +0200

    Added styles and script to task-4.js.

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+'use strict'
+const registerForm = document.querySelector('.login-form');
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const email = this.elements.email.value.trim();
+  const password = this.elements.password.value.trim();
+  
+  if (email === '' || password === '') {
+    alert ('All form fields must be filled in');
+  } else {
+    const formData = {
+        email: email,
+        password: password,
+    };
+
+    console.log(formData);
+    this.reset();
+  };
+}
