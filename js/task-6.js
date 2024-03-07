1969bea50db10c59d7c7bc74f06920430fbca7c7
Marianna Dei <d.marianna@icloud.com>
2024-03-07 23:52:54 +0200

    Added styles and script to task-6.js.

---
+++ js/task-6.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+  for (let i = 0; i < amount; i++) {
+    const divElement = document.createElement('div');
+    divElement.style.width = `${30 + i * 10}px`;
+    divElement.style.height = `${30 + i * 10}px`;
+    divElement.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(divElement);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.querySelector('#boxes');
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
