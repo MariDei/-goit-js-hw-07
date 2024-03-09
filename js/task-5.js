7f3487c9598df57df425ec0a0e4bd4ed1f2b1e62
Marianna Dei <d.marianna@icloud.com>
2024-03-09 23:08:33 +0200

    Fixed code for task-5.js.

--- js/task-5.js
+++ js/task-5.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const button = document.querySelector('button.change-color');
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+const button = document.querySelector('button.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
 function changeBackgroundColor() {
   const color = getRandomHexColor();
-  document.body.style.backgroundColor = color;
-  document.querySelector('.color').textContent = color;
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
 }
 
 button.addEventListener('click', changeBackgroundColor);
